fe31d58e63b1c2c76cbeed57d9d184ce803801fd
Shrey Aeron <shreystechtips@gmail.com>
2018-06-12 21:45:48 -0700

    Testing new ideas....

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var $;
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
